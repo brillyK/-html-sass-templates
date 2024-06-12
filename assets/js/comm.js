e5548e99f780cb5fa400ae4c45cfd43b1a327551
brillyK <rkdud9798@naver.com>
2024-06-12 16:44:11 +0900

    ll

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,37 +1,48 @@
-var swiper = new Swiper('.slide-intro', {
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
-    // autoplay: {
-    //     delay: 3000,
-    //     disableOnInteraction: false,
-    // },
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-})
+const headerEl = document.querySelector('#header')
+let lastScrollY = 0
 
-var swiper = new Swiper('.slide-eft .swiper', {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-        },
-        1280: {
-            slidesPerView: 4,
-            spaceBetween: 24,
-        },
-    },
-})
+window.addEventListener(
+    'scroll',
+    //쓰로틀 사용 시 로대쉬 cbn 넣어야함
+    _.throttle(function () {
+        //현재 스크롤 위치
+        const currentScrollY = window.scrollY
+        console.log('현재 스크롤 위치 :' + currentScrollY)
+        console.log('마지막 스크롤 위치 :' + lastScrollY)
+
+        if (currentScrollY > 200) {
+            //#header가 bg-w 클래스 추가한다 add / 뺀다 remove
+            headerEl.classList.add('bg-w')
+
+            gsap.to(headerEl, {
+                backdropFilter: 'blur(30px)',
+                duration: 0.5,
+            })
+
+            //스크롤 방향에 따라 헤더 숨김,보임
+            if (currentScrollY > lastScrollY) {
+                //스크롤 다운 시
+                gsap.to(headerEl, {
+                    y: -100,
+                    duration: 0.5,
+                })
+            } else {
+                //스크롤 업 시
+                gsap.to(headerEl, {
+                    y: 0,
+                    duration: 0.5,
+                })
+            }
+        } else {
+            headerEl.classList.remove('bg-w')
+
+            gsap.to(headerEl, {
+                backgroundColor: 'transparent',
+                backdropFilter: 'blur(0)',
+                duration: 0.5,
+            })
+        }
+        //마지막 스크롤 위치 갱신
+        lastScrollY = currentScrollY
+    }, 300),
+)
